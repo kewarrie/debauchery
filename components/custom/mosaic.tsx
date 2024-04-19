da154f1045a00a9ef3dc86fa766d58eb50ee0d0b
Kevin <65783003+kewarrie@users.noreply.github.com>
2024-04-19 17:37:32 +0300

    Setup drawer, theme switcher, favicon, etc.

--- components/custom/mosaic.tsx
+++ components/custom/mosaic.tsx
@@ -18,6 +18,7 @@ export default function Mosaic({ tesserae }: TesseraeProps) {
 
   return (
     <>
+      <Separator className="border border-red-600 dark:border-red-950" />
       <p className="block h-full w-full text-center uppercase text-red-950 dark:text-red-100 font-mono my-4">
         debauchery
       </p>
@@ -32,11 +33,18 @@ export default function Mosaic({ tesserae }: TesseraeProps) {
         </div>
       </div>
       <div className="container mx-auto p-4">
-        <div className="h-80 flex items-center justify-center">
-            <Switch />
+        <div className="h-40 flex items-center justify-center">
+          <Switch />
         </div>
       </div>
-      
+      <Separator className="border border-red-600 dark:border-red-950" />
+      <p className="block h-full w-full text-center uppercase text-red-950 dark:text-red-100 font-mono my-4 text-sm">
+        both alcohol and smoking can cause cancer
+      </p>
+      <p className="block h-full w-full text-center uppercase text-red-950 dark:text-red-100 font-mono my-4 text-xs">
+        use responsibly
+      </p>
+      <Separator className="border border-red-600 dark:border-red-950" />
     </>
   );
 }
--- components/custom/tile.tsx
+++ components/custom/tile.tsx
@@ -5,6 +5,8 @@
 "use client";
 
 import type { TesseraProps } from "@/lib/utils";
+import{ Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+
 
 import dayjs from "dayjs";
 
@@ -13,22 +15,28 @@ interface TileProps {
 }
 
 export default function Tile({ tessera }: TileProps) {
-
-  // console.log(tessera);
-
+  
   let month: string = dayjs(tessera.period).format('MMMM');
   let year: string = dayjs(tessera.period).format('YYYY');
 
   return (
     <div className="p-4 rounded-none border-2 border-red-600 hover:bg-red-100 dark:border-red-950 h-80 dark:hover:bg-red-950">
       <div className="block h-full w-full">
-        <p className="block h-full w-full text-center uppercase text-red-950 dark:text-red-100 font-mono">
-          <a href="#" className="block h-full w-full content-center bg-white dark:bg-black">
+        <Sheet>
+          <SheetTrigger className="block h-full w-full content-center text-red-950 dark:text-red-100 bg-white dark:bg-black font-mono uppercase">
             {month}
             <br />
             {year}
-          </a>
-        </p>
+          </SheetTrigger>
+          <SheetContent className="w-[100%] sm:w-[640px] md:w-[768px] lg:w-[1024px] dark:bg-stone-950 border-red-950 sm:max-w-none md:max-w-none lg:max-w-none xl:max-w-none">
+            <SheetHeader>
+              <SheetTitle className="text-red-950 dark:text-red-100 font-mono uppercase">{month} {year}</SheetTitle>
+              <SheetDescription >
+                <p>Content</p>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
       </div>
     </div>
   );
