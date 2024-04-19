5b05283cd02e2e790c99a9463bbfc4ae17ec6091
Kevin <65783003+kewarrie@users.noreply.github.com>
2024-04-19 19:36:29 +0300

    Update Switch function

--- components/custom/switch.tsx
+++ components/custom/switch.tsx
@@ -18,26 +18,14 @@ export default function Switch() {
 
   return (
     <>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon" className="border-2 border-red-600 dark:border-red-950 rounded-full focus-visible:ring-red-600 dark:focus-visible:ring-red-950 hover:bg-none dark:hover:bg-none">
-            <Sun className="stroke-red-600 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="stroke-red-950 absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end" className="text-red-600 dark:text-red-100">
-          <DropdownMenuItem className="focus:bg-red-100 dark:focus:bg-red-950 focus:text-red-600 dark:focus:text-red-100 font-mono uppercase" onClick={() => setTheme("light")}>
-            Light
-          </DropdownMenuItem>
-          <DropdownMenuItem className="focus:bg-red-100 dark:focus:bg-red-950 focus:text-red-600 dark:focus:text-red-100 font-mono uppercase" onClick={() => setTheme("dark")}>
-            Dark
-          </DropdownMenuItem>
-          <DropdownMenuItem className="focus:bg-red-100 dark:focus:bg-red-950 focus:text-red-600 dark:focus:text-red-100 font-mono uppercase" onClick={() => setTheme("system")}>
-            System
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <Button variant="outline" size="icon" className="border-2 border-red-600 dark:border-red-950 rounded-full focus-visible:ring-red-600 dark:focus-visible:ring-red-950 hover:bg-none dark:hover:bg-none visible dark:hidden" onClick={() => setTheme("dark")}>
+        <Sun className="stroke-red-600 h-[1.2rem] w-[1.2rem]" />
+        <span className="sr-only">Toggle theme</span>
+      </Button>
+      <Button variant="outline" size="icon" className="border-2 border-red-600 dark:border-red-950 rounded-full focus-visible:ring-red-600 dark:focus-visible:ring-red-950 hover:bg-none dark:hover:bg-none hidden dark:flex" onClick={() => setTheme("light")}>
+        <Moon className="stroke-red-950 absolute h-[1.2rem] w-[1.2rem]" />
+        <span className="sr-only">Toggle theme</span>
+      </Button>
     </>
   );
 }
