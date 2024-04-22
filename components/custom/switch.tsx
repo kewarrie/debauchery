5babb3c66adcfdc4809e3e79dab657ade0386bf7
Kevin <65783003+kewarrie@users.noreply.github.com>
2024-04-22 21:14:40 +0300

    Clean up

--- components/custom/switch.tsx
+++ components/custom/switch.tsx
@@ -9,7 +9,6 @@ import * as React from "react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 import { Moon, Sun } from "lucide-react";
 
